700635b4b8f3b2ad7384ffd011d6708df0cf223a
moehtet <laxus3331@gmail.com>
2020-10-29 19:43:45 +0630

    wip main screen

--- app/src/main/java/com/dazai/yukino/MainActivity.kt
+++ app/src/main/java/com/dazai/yukino/MainActivity.kt
@@ -2,8 +2,21 @@ package com.dazai.yukino
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dazai.yukino.fragments.OfferFragment
+import com.dazai.yukino.fragments.ProfileFragment
+import com.dazai.yukino.fragments.RestaurantFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val restaurantFragment = RestaurantFragment()
+    private val profileFragment = ProfileFragment()
+    private val offerFragment = OfferFragment()
+
+    val fragmentManager = supportFragmentManager
+
+    val active = restaurantFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

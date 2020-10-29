ba59c35ca439bc2362a82d3168bd3fd245aaf25d
moehtet <laxus3331@gmail.com>
2020-10-29 19:52:27 +0630

    bottom navigation done'

--- app/src/main/java/com/dazai/yukino/MainActivity.kt
+++ app/src/main/java/com/dazai/yukino/MainActivity.kt
@@ -2,9 +2,12 @@ package com.dazai.yukino
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.dazai.yukino.fragments.OfferFragment
 import com.dazai.yukino.fragments.ProfileFragment
 import com.dazai.yukino.fragments.RestaurantFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +17,37 @@ class MainActivity : AppCompatActivity() {
 
     val fragmentManager = supportFragmentManager
 
-    val active = restaurantFragment
+    var active : Fragment = restaurantFragment
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentManager.beginTransaction().add(R.id.fragmentContainer,offerFragment).hide(offerFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.fragmentContainer,profileFragment).hide(profileFragment).commit()
+        fragmentManager.beginTransaction().add(R.id.fragmentContainer,restaurantFragment).commit()
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.actionOffer -> {
+                    fragmentManager.beginTransaction().hide(active).show(offerFragment).commit()
+                    active = offerFragment
+                    true
+                }
+                R.id.actionProfile -> {
+                    fragmentManager.beginTransaction().hide(active).show(profileFragment).commit()
+                    active = profileFragment
+                    true
+                }
+                R.id.actionRestaurant -> {
+                    fragmentManager.beginTransaction().hide(active).show(restaurantFragment).commit()
+                    active = restaurantFragment
+                    true
+                }
+               else -> true
+            }
+        }
+
     }
 }

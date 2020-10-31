6782390b4d1641f9a9ea44739e8f426780b1ed21
moehtet <laxus3331@gmail.com>
2020-10-31 17:27:00 +0630

    onboard screen one done

---
+++ app/src/main/java/com/dazai/yukino/activities/OnboardScreenTwo.kt
@@ -0,0 +1,12 @@
+package com.dazai.yukino.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dazai.yukino.R
+
+class OnboardScreenTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard_screen_two)
+    }
+}
